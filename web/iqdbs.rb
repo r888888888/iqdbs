e9e1bad972be53402d9047a55e5c4a0858291b5c
Albert Yi <albert@carbonfive.com>
2018-06-15 12:44:16 -0700

    support callbacks

--- web/iqdbs.rb
+++ web/iqdbs.rb
@@ -10,31 +10,50 @@ require "iqdb/responses/error"
 require "iqdb/responses/responses"
 require "iqdb/server"
 require "iqdb/command"
+require "acti"
 
 set :port, ENV["SINATRA_PORT"]
 
-before "/similar" do
-  if params["key"] != ENV["AUTH_KEY"]
-    halt 401
+# before "/similar" do
+#   if params["key"] != ENV["AUTH_KEY"]
+#     halt 401
+#   end
+# end
+
+def find_referer(url)
+  if url =~  /\Ahttps?:\/\/(?:\w+\.)?pixiv\.net/ || url =~ /\Ahttps?:\/\/i\.pximg\.net/
+    return "https://www.pixiv.net"
   end
+
+  if url =~ %r{https?://lohas\.nicoseiga\.jp} || url =~ %r{https?://seiga\.nicovideo\.jp}
+    return "https://seiga.nicovideo.jp"
+  end
+
+  return nil
 end
 
 post "/similar" do
-  file = params["file"]
   server = Iqdb::Server.default
-  begin
-    server.query(5, file["tempfile"].path).to_json
-  rescue Iqdb::Responses::Error => e
-    JSON.generate({"error" => e.to_s})
-  end
-end
 
-get "/similar" do
-  url = params["url"]
-  ref = params["ref"]
-  server = Iqdb::Server.default
   begin
-    server.download_and_query(url, ref, 5).to_json
+    if params["file"]
+      file = params["file"]
+      results = server.query(5, file["tempfile"].path)
+      
+      if params["callback"]
+        data = results.matches.map {|x| [x.post_id, x.score]}.to_json
+        url = URI::HTTP.build(host: params["callback"], query: URI.encode_www_form({matches: data}))
+        redirect url.to_s
+      else
+        results.to_json
+      end
+
+    elsif params["url"]
+      url = params["url"]
+      ref = params["ref"] || find_referer(url)
+      server.download_and_query(url, ref, 5).to_json
+    end
+
   rescue Iqdb::Responses::Error => e
     JSON.generate({"error" => e.to_s})
   end
